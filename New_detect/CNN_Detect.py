1b5bc0f18193dbe6ebc59cc895ab22071337b258
ranai-srivastav <sweetynranai@gmail.com>
2022-04-15 10:10:44 -0500

    activation keyword not understood??

--- New_detect/CNN_Detect.py
+++ New_detect/CNN_Detect.py
@@ -1,16 +1,23 @@
 import json
-import cv2
 import os
 
-with open("/home/ranais/Ranai/Research/object-detection-ex-template/Training/annotation-20220413T205639Z-001/annotation/final_anns.json", "r") as annotn_file:
+import cv2
+import numpy as np
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.models import Sequential
+
+with open(
+        "/home/ranais/Ranai/Research/object-detection-ex-template/Training/annotation-20220413T205639Z-001/annotation/final_anns.json",
+        "r") as annotn_file:
     annotn = json.load(annotn_file)
 
 root_dir = "/home/ranais/Ranai/Research/object-detection-ex-template/Training/frames-20220412T154427Z-001/frames"
 imgs = os.listdir(root_dir)
 training_image_set = []
 training_annotn_set = []
+
 for img in imgs:
-    training_image_set.append(cv2.imread(root_dir+"/"+img))
+    training_image_set.append(cv2.imread(root_dir + "/" + img))
     found_duck = False
     for dict_item in annotn[img]:
         if dict_item["cat_id"] == 2:
@@ -23,3 +30,48 @@ for img in imgs:
 # for i in range(9):
 #     cv2.imshow(f"{imgs[i]} + {training_annotn_set[i]}", training_image_set[i])
 #     cv2.waitKey(1000)
+
+training_image_array = np.array(training_image_set)
+print(training_image_array.shape)
+training_annotn_array = np.array(training_annotn_set)
+# training_image_array = training_image_array[:, :, 1]
+training_image_array.reshape((training_image_array.shape[0], 960, 960, 1))
+cv2.imshow("img", training_image_array[179])
+cv2.waitKey(1000)
+
+
+def CNN(x_train, y_train, x_test, y_test):
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(448, 448, 1)))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (1, 1), activaiton='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(Conv2D(128, (1, 1), activation='relu'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(Conv2D(256, (1, 1), activation='relu'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(Conv2D(256, (1, 1), activation='relu'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Conv2D(1024, (3, 3), activation='relu'))
+    model.add(Conv2D(512, (1, 1), activation='relu'))
+    model.add(Conv2D(1024, (3, 3), activation='relu'))
+    model.add(Conv2D(512, (1, 1), activation='relu'))
+    model.add(Conv2D(1024, (3, 3), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=10, batch_size=32, verbose=0)
+
+CNN(training_image_array, training_annotn_array, None, None)
+
+
