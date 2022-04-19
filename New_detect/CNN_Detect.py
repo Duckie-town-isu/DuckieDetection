03ff6cefe9eecba31db286d812570fc4c140287a
ranai-srivastav <sweetynranai@gmail.com>
2022-04-19 10:40:09 -0500

    Saving the model

--- New_detect/CNN_Detect.py
+++ New_detect/CNN_Detect.py
@@ -73,6 +73,10 @@ def CNN(x_train, y_train, x_test, y_test):
     model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.fit(x_train, y_train, epochs=10, batch_size=32, verbose=0)
 
+    model.save_weights('CNN_Detect.h5')
+    model.save('CNN_Detect.h5')
+
+
 CNN(training_image_array, training_annotn_array, None, None)
 
 
